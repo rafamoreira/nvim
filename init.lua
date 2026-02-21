a3f8005d0bd15c46f3e4fc75a9f55ac432155539
Rafael M Cunha <me@rafaelmc.net>
2026-02-22 00:33:51 +0100

    add treesitter and structure

--- init.lua
+++ init.lua
@@ -1,15 +1,29 @@
--- Load options, keymaps, autocmds first
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
+-- ./init.lua
 
--- Bootstrap lazy.nvim
+-- 1. Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none",
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath })
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- 2. Map leader key BEFORE loading lazy (very important!)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- 3. Setup lazy and tell it to load the `lua/plugins/` directory
 require("lazy").setup("plugins")
+
+
+-- Load options, keymaps, autocmds first
+-- require("config.options")
+-- require("config.keymaps")
+-- require("config.autocmds")
+
